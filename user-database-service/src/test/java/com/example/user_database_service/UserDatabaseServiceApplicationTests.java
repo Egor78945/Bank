2ac29bc4e0ae710e174a5eeef2f5c0222929b9f3
egor <Egor78396@gmail.com>
2025-01-09 17:03:08 +0300

    user-database-service created

---
+++ user-database-service/src/test/java/com/example/user_database_service/UserDatabaseServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.user_database_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UserDatabaseServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
