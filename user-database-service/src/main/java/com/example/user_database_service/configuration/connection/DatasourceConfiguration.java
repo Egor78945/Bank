69c3a9a20630623f434e468a826f248532d7c50a
egor <Egor78396@gmail.com>
2025-01-11 02:27:46 +0300

    user-database-service(liquibase migration)

---
+++ user-database-service/src/main/java/com/example/user_database_service/configuration/connection/DatasourceConfiguration.java
@@ -0,0 +1,21 @@
+package com.example.user_database_service.configuration.connection;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.jdbc.DataSourceBuilder;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import javax.sql.DataSource;
+
+@Configuration
+public class DatasourceConfiguration {
+    @Bean
+    public DataSource dataSource(@Value("${spring.datasource.url}") String url, @Value("${spring.datasource.username}") String username, @Value("${spring.datasource.password}") String password, @Value("${spring.datasource.driver-class-name}") String driverClassName) {
+        return DataSourceBuilder.create()
+                .url(url)
+                .username(username)
+                .password(password)
+                .driverClassName(driverClassName)
+                .build();
+    }
+}
